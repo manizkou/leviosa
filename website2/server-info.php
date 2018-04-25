9a3bbceb3b9cfbec3e40ae8ad51471d4e0ea1963
manizkou <alertmanishrana@gmail.com>
2018-04-25 20:59:18 +0545

    server-info changes

--- website2/server-info.php
+++ website2/server-info.php
@@ -3,11 +3,22 @@
 	//create server array
 	
 	$server = [
-		'Host Server Name' =>$_SERVER['SERVER_NAME']
+		'Host Server Name' =>$_SERVER['SERVER_NAME'],
+		'Host Header'=>$_SERVER['HTTP_HOST'],
+		'Server Software' =>$_SERVER['SERVER_SOFTWARE'],
+		'Document root' => $_SERVER['DOCUMENT_ROOT'],
+		'Script Name' => $_SERVER['SCRIPT_NAME'],
+		'Script Filename'=> $_SERVER['SCRIPT_FILENAME']
 	];
 
-	echo $server['Host Server Name'];
+	// echo $server['Server Software'];
+	// print_r($server);
 
 	//create client array
+	$client = [
+		'Client System Info'=>$_SERVER['HTTP_USER_AGENT']
+	];
+
+	print_r($client);
 
 ?>
